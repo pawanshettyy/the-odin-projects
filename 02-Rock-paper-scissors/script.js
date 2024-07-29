2d84aa28d04dcab1fb60dde0970c72ca79292455
Pawan Shetty <53149315+pawanshettyy@users.noreply.github.com>
2024-07-29 11:00:55 +0530

    final project file
    completed project 2 of odin project - rock , paper , scissors

---
+++ 02-Rock-paper-scissors/script.js
@@ -0,0 +1,83 @@
+//console.log function to test 
+//console.log("Hello World")
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    }
+    else if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')
+    ) {
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == 'Tie') {
+        return "Its a tie!"
+    }
+    else if (result == "Player") {
+        return `You win ${playerSelection} beats ${computerSelection}`
+    }
+    else {
+        return `You lose ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+function getPlayerChoice() {
+    let validatedInput = false;
+    while (validatedInput == false) {
+        const choice = prompt("Rock Paper Scissors");
+        if (choice == null) {
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)) {
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!");
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if (checkWinner(playerSelection, computerSelection) == "Player") {
+            scorePlayer++;
+        }
+        else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+            scoreComputer++
+        }
+    }
+    console.log("Game Over")
+    if(scorePlayer > scoreComputer){
+        console,log("Player was the winner")
+    }
+    else if(scorePlayer < scoreComputer){
+        console,log("Computer was the winner")
+    }
+    else{
+        console,log("we have a tie")
+    }
+    
+}
+
+game()
+
