3d3869a93a85b5364cd664a762a6739bdb6c8060
pawanshettyy <pawanshetty13092006@gmail.com>
2024-08-14 07:30:16 +0000

    Html completed

---
+++ 06-Library/script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book() {}
+
+function addBookToLibrary() {
+  //do stuff here
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function () {
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = block;
+});
